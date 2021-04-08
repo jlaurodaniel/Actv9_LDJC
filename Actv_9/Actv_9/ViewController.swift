6b3247fae43e682fcbd7f12efd49994f40e27073
Lauro Daniel Jimenez Custodio <47502588+jlaurodaniel@users.noreply.github.com>
2021-04-07 20:09:57 -0400

    Add files via upload

---
+++ Actv_9/Actv_9/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Actv_9
+//
+//  Created by user182933 on 4/7/21.
+//  Copyright © 2021 user182933. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Actv_9/Actv_9Tests/Actv_9Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Actv_9Tests.swift
+//  Actv_9Tests
+//
+//  Created by user182933 on 4/7/21.
+//  Copyright © 2021 user182933. All rights reserved.
+//
+
+import XCTest
+@testable import Actv_9
+
+class Actv_9Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
